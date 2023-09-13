b6a2fc2c864a29f1c5e3e73184411845b863da33
Iamneo <techadmin@iamneo.ai>
2023-09-13 12:22:00 +0000

    Backup Commit

--- my-spring-project/src/main/java/com/example/App.java
+++ my-spring-project/src/main/java/com/example/App.java
@@ -33,20 +33,6 @@ public class App {
             props.put("mail.smtp.auth", "true");
             props.put("mail.smtp.starttls.enable", "true");
 
-            Session session = Session.getInstance(props, new Authenticator() {
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication(fromEmail, password);
-                }
-            });
-
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(fromEmail));
-            message.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse(toEmail));
-            message.setSubject(subject);
-            message.setText(messageText);
-
-            Transport.send(message);
             return true;
         } catch (Exception e) {
             e.printStackTrace();
